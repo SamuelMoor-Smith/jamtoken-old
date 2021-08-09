34749c7baec3a838470f523e904f274078e4ad1f
JustinRyukiRogers <jr.rogers@mail.utoronto.ca>
2021-08-09 11:24:03 -0400

    contracts (donations + JamToken (sushiswap))

--- jamtoken-app/src/components/Charity.js
+++ jamtoken-app/src/components/Charity.js
@@ -1,13 +1,14 @@
 import CharityImage from './CharityImage';
 import CharityButton from './CharityButton';
+import style from './Charity.module.css';
 
 var React = require('react');
 
 class Charity extends React.Component {
     render() {
         return (
-            <div>
-                <h1>{this.props.title}</h1>
+            <div className = {style.Charity}>
+                <h1 >{this.props.title}</h1>
                 <CharityImage picture={this.props.picture}/>
                 <p> {this.props.blurb}</p>
                 {/* <CharityImage url={this.props.props.picture}></CharityImage> */}
